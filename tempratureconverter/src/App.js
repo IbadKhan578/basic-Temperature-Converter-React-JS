ff01443f2acd183d6c1161c9ea655db2adc5eae5
Ibad Khan <ibadkhansherani@gmail.com>
2025-06-17 04:48:24 -0700

    initial commit

---
+++ tempratureconverter/src/App.js
@@ -0,0 +1,10 @@
+import Converter from './components/Converter';
+import './components/converter.css';
+
+function App() {
+  return (
+<Converter/>
+  );
+}
+
+export default App;
---
+++ tempratureconverter/src/components/Converter.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function Converter(){
+
+    let[celsius,Setcelsius] = useState("");
+    let [farenheit,SetFarenheit]=useState("")
+
+    function handleCelsius(e){
+        const value = e.target.value;
+        Setcelsius(value);
+        SetFarenheit(value===''?'':  (value*(9/5)+32) );
+       
+
+    }
+
+     function handleFarehnheit(e){
+        const value = e.target.value;
+        SetFarenheit(value);
+        Setcelsius(value===''?'': (((value-32)*5)/9) )  //((value - 32) * 5) / 9
+     
+
+    } //(32°F − 32) × 5/9
+
+    return(
+        <div className="converter">
+            <h1> Temprature Converter</h1>  
+            <label className='calsius'>Calsius
+                <input type="number"  value={celsius} placeholder='0'
+                onChange={handleCelsius} />
+
+            </label>
+
+              <label >Farenheit
+                <input type="number"  value={farenheit} placeholder='32' 
+                onChange={handleFarehnheit} />
+
+            </label>
+            
+        </div>
+
+
+    )
+
+
+}
+
+export default Converter;
